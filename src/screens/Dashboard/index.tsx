18fd166ce03e9a42c4a083958347e1a1ccbe2b8d
fabianoobispo <fbc623@gmail.com>
2021-07-28 22:59:53 -0300

    cards

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {StatusBar} from 'react-native';
 import { HighlightCard } from '../../components/HighlightCard';
 
 import { 
@@ -11,12 +11,16 @@ import {
     User,
     UserGreeting,
     UserName,
-    Icon
+    Icon,
+    HighlightCards,
+ 
   } from './styles';
 
 export function Dashboard(){
     return (
+        
         <Container>
+            <StatusBar barStyle={'light-content'} />
         <Header>
         <UserWrapper>
             <UserInfo>
@@ -25,14 +29,36 @@ export function Dashboard(){
                 />
                 <User>
                     <UserGreeting>Olá,</UserGreeting>
-                    <UserName>Rodrigo</UserName>
+                    <UserName>Fabiano</UserName>
                 </User>
             </UserInfo>
           <Icon name="power"/>
         </UserWrapper>
      
         </Header>
-        <HighlightCard/>
+
+        <HighlightCards>
+            <HighlightCard
+                type='up'
+                title="Entradas"
+                amount="R$ 17.400.00"
+                lastTransaction="última entrada dia 13 de abril"
+            />
+               <HighlightCard
+                type='down'
+                title="Saídas"
+                amount="R$ 1.400.00"
+                lastTransaction="última saída dia 13 de abril"
+            />
+               <HighlightCard
+                type='total'
+                title="Total"
+                amount="R$ 16.400.00"
+                lastTransaction="01 á 13 de abril"
+            />
+          
+        </HighlightCards>
+      
    
         </Container>
     )
